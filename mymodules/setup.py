c0e01612dd23deb405cebdb50e9b13fe0cdf2646
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2021-01-18 11:18:06 -0600

    capitulo del 1 al 5

---
+++ mymodules/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vsearch',
+    version='1.0',
+    description='The head First Python Search Tools',
+    author='HF Python 2e',
+    author_email='lourdespizarro@gmail.com',
+    url='headfirstlabs.com',
+    py_modules=['vsearch'],
+)
