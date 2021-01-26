577713eb56437b2b42f1740b67bc81b39084669c
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2021-01-26 17:07:54 -0600

    respaldo srt

---
+++ countfromby.py
@@ -0,0 +1,12 @@
+class CountFromBy:
+
+    def _init_ (self, v: int, i: int) -> None:
+        self.val = v
+        self.incr = i
+
+    def increase(self) -> None:
+        self. val += self.incr
+
+
+
+        
