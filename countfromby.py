9128072289acd1fbeb37c898958c73d791ea8a0f
lpizarro <lpizarro2391@github.com>
2021-02-14 19:11:28 -0600

    inicializando las variables y poniendole nombre

--- countfromby.py
+++ countfromby.py
@@ -1,11 +1,9 @@
 class CountFromBy:
-
-    def _init_ (self, v: int, i: int) -> None:
-        self.val = v
-        self.incr = i
-
+    def _init_(self, v: int, i: int)  -> None:
+         self.val = v
+         self.incr = i
     def increase(self) -> None:
-        self. val += self.incr
+        self.val += self.incr
 
 
 
---
+++ def_soundbite.py
@@ -0,0 +1,4 @@
+def soundbite(from_outside):
+    insider = 'JAMES'
+    outsider = from_outside
+    print(from_outside, insider , outsider)
