b30a03b3c7c1e008524ac3b7bf3aea39f57119ec
lpizarro <lpizarro2391@github.com>
2021-03-09 22:02:15 -0600

    respaldo mac

--- countfromby.py
+++ countfromby.py
@@ -1,11 +1,14 @@
 class CountFromBy:
     
-    def __init__(self, v: int, i: int)  -> None:
+    def __init__(self, v: int=0, i: int=1)  -> None:
          self.val = v
          self.incr = i
          
     def increase(self) -> None:
         self.val += self.incr
+
+    def __repr__(self) ->str:
+        return str(self.val)
 
 
 
